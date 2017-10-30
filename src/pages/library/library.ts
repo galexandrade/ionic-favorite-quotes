06671acf483cb750edac089eb42a17a2143c502d
Alex Andrade <g.alex.andrade@gmail.com>
2017-10-30 17:37:36 -0200

    First commit

---
+++ src/pages/library/library.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../../data/quote.interface';
+import quotes from '../../data/quotes';
+import { QuotesPage } from '../quotes/quotes';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html',
+})
+export class LibraryPage implements OnInit {
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
+  quotesPage: QuotesPage;
+
+  constructor(private navCtrl: NavController){
+  }
+
+  ngOnInit(){
+    this.quoteCollection = quotes;
+  }
+
+  onGoToQuotesPage(quoteGroup){
+    this.navCtrl.push(QuotesPage, quoteGroup);
+  }
+}
---
+++ src/pages/quotes/quotes.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage implements OnInit {
+  quoteGroup: {category: string, quotes: Quote[], icon: string}
+
+  constructor(private navParams: NavParams){
+
+  }
+
+  ngOnInit(){
+    this.quoteGroup = this.navParams.data;
+  }
+
+  /* // Add elvis operator (?) in template to use this approach
+  ionViewDidLoad(){
+    this.quoteGroup = this.navParams.data;
+  }
+  */
+
+}
